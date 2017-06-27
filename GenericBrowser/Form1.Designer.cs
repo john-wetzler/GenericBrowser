1d25f34e3d24639df8d8910eea692c9b704b423f
John L. Wetzler <jwetzler@cityofstrife.net>
2017-06-27 10:09:49 -0400

    Add project files.

---
+++ GenericBrowser/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace GenericBrowser
+{
+	partial class BrowserWindow
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.goButton = new System.Windows.Forms.Button();
+			this.inputBox = new System.Windows.Forms.TextBox();
+			this.renderBox = new System.Windows.Forms.GroupBox();
+			this.SuspendLayout();
+			// 
+			// goButton
+			// 
+			this.goButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.goButton.BackColor = System.Drawing.Color.LimeGreen;
+			this.goButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.goButton.Location = new System.Drawing.Point(740, 5);
+			this.goButton.Name = "goButton";
+			this.goButton.Size = new System.Drawing.Size(35, 23);
+			this.goButton.TabIndex = 1;
+			this.goButton.Text = "Go";
+			this.goButton.UseVisualStyleBackColor = false;
+			this.goButton.Click += new System.EventHandler(this.goButton_Click);
+			// 
+			// inputBox
+			// 
+			this.inputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.inputBox.Location = new System.Drawing.Point(10, 5);
+			this.inputBox.Name = "inputBox";
+			this.inputBox.Size = new System.Drawing.Size(725, 20);
+			this.inputBox.TabIndex = 2;
+			this.inputBox.Text = "http://";
+			// 
+			// renderBox
+			// 
+			this.renderBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.renderBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.renderBox.Location = new System.Drawing.Point(10, 34);
+			this.renderBox.Name = "renderBox";
+			this.renderBox.Size = new System.Drawing.Size(765, 525);
+			this.renderBox.TabIndex = 3;
+			this.renderBox.TabStop = false;
+			this.renderBox.Enter += new System.EventHandler(this.renderBox_Enter);
+			// 
+			// BrowserWindow
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoScroll = true;
+			this.BackColor = System.Drawing.SystemColors.ControlLight;
+			this.ClientSize = new System.Drawing.Size(784, 561);
+			this.Controls.Add(this.renderBox);
+			this.Controls.Add(this.inputBox);
+			this.Controls.Add(this.goButton);
+			this.MaximizeBox = false;
+			this.Name = "BrowserWindow";
+			this.Text = "Web Browser";
+			this.Load += new System.EventHandler(this.BrowserWindow_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Button goButton;
+		private System.Windows.Forms.TextBox inputBox;
+		private System.Windows.Forms.GroupBox renderBox;
+	}
+}
+
---
+++ GenericBrowser/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Http;
+
+namespace GenericBrowser
+{
+	public partial class BrowserWindow : Form
+	{
+		public BrowserWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void BrowserWindow_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void renderBox_Enter(object sender, EventArgs e)
+		{
+
+		}
+
+		private void goButton_Click(object sender, EventArgs e)
+		{
+			string strURL = formatURL(inputBox.Text);
+			this.Text = strURL; // Default to URL if no <title> tag found
+
+			HttpClient client = new HttpClient();
+			HttpContent content = client.GetAsync(strURL).Result.Content;
+			renderContent(content.ReadAsStringAsync().Result);
+		}
+
+		private string formatURL(string strURL)
+		{
+			if(strURL.StartsWith("http://",StringComparison.InvariantCultureIgnoreCase) == false)
+			{   // Correct the formatting
+				strURL = string.Concat("http://", strURL);
+			}
+
+			return strURL;
+		}
+
+		private void renderContent(string html)
+		{
+			// Start with the <head> processing
+			//-----------------------------------------------------------------
+			int start = html.IndexOf("<head>");
+			if(start > -1)
+			{   // <head> tag located!
+				start += 6; // We don't actually NEED the <head> part
+				int end = html.IndexOf("</head>");
+				if (end > start)
+				{	// There's text between opening and closing tags
+					string header = html.Substring(start, end - start);
+					parseHead(header);
+				}
+			}
+
+			// Now move onto the <body> processing
+			//-----------------------------------------------------------------
+			start = html.IndexOf("<body>");
+			if (start > -1)
+			{   // <body> tag located!
+				start += 6; // As before, we don't need the tag part itself
+				int end = html.IndexOf("</body>");
+				if (end > start)
+				{   // There's text between opening and closing tags
+					string body = html.Substring(start, end - start);
+					parseBody(body.Trim());
+				}
+			}
+		}
+
+		//---------------------------------------------------------------------------
+		private void parseHead(string html)
+		{	// Strip out the "title" and set the window's title to it
+			int start = html.IndexOf("<title>");
+			if (start > -1)
+			{   // <title> tag located!
+				start += 7; // Again, we don't need the opening tag part
+				int end = html.IndexOf("</title>");
+				if (end > start)
+				{   // There's text between opening and closing tags
+					string title = html.Substring(start, end - start);
+					this.Text = title; // Set window title to HTML <title> content
+				}
+			}
+		}
+
+		//---------------------------------------------------------------------------
+		// Valid HTML tags to parse, per instructions:
+		//static readonly string[] VALID_TAGS = "h1,h2,h3,h4,div,p,span,a,table,tr,td,th".Split(',');
+
+		//---------------------------------------------------------------------------
+		private void parseBody(string html)
+		{
+			string finalHTML = "";
+
+			while (html.Length > 0)
+			{
+				html = html.Trim();
+				int start = html.IndexOf("<");
+				if (start == -1)
+				{   // There are no tags on this line, just return the string
+					finalHTML = html.Trim() + " ";
+					html = "";
+				}
+				else
+				{   // First, keep any text prior to the first bracket
+					// (for things like mid-line HTML tags)
+					if (start > 0)
+					{
+						finalHTML += html.Substring(0, start) + " ";
+						html = html.Substring(start); // Then drop the processed part
+					}
+
+					// Get the tag!
+					int end = html.IndexOf(">");
+					string tagContent = html.Substring(1, end - 1);
+					finalHTML += processTag(tagContent);
+					html = html.Substring(end + 1);
+				}
+			}
+
+			finalHTML = finalHTML.Replace("  ", " ").Trim(); // Get rid of any double-spaces & leading whitespace
+			renderBox.Text = finalHTML;
+		}
+
+		//---------------------------------------------------------------------------
+		private string processTag(string tag)
+		{
+			string retVal = "";
+			bool bClosing = false;
+
+			if (tag[0] == '/')
+			{
+				tag = tag.Substring(1);
+				bClosing = true;
+			}
+
+			string[] tagArray = tag.Split();
+
+			switch( tagArray[0] )
+			{
+				case "h1":
+				case "h2":
+				case "h3":
+				case "h4":
+					retVal += "\n";
+					if(bClosing)
+						retVal += "\n";
+					break;
+
+				case "div":
+					retVal += "\n";
+					if (bClosing)
+						retVal += "\n";
+					break;
+
+				case "p":
+					retVal += "\n";
+					if (bClosing)
+						retVal += "\n";
+					break;
+
+				case "br":
+					retVal += "\n";
+					break;
+
+				default:
+					break;
+			}
+
+			return retVal;
+		}
+	}
+}
